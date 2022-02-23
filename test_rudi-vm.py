16d84aada0233a9dd9981e0522bf4a6812178e13
Tobias Groß <gitlab@toerb.de>
2022-02-23 23:34:59 +0100

    add more website checks

--- test_rudi-vm.py
+++ test_rudi-vm.py
@@ -15,3 +15,44 @@ class TestHTTPS(unittest.TestCase):
         r = requests.get('https://matrix.toerb.de:8448/_matrix/federation/v1/version')
         self.assertTrue(r.ok)
         self.assertIn('server', r.json())
+
+    def test_teslamate(self):
+        r = requests.get('https://teslamate.toerb.de/')
+        self.assertFalse(r.ok)
+        self.assertEqual(r.status_code, 404)
+
+    def test_nextcloud(self):
+        r = requests.get('https://cloud.toerb.de/')
+        self.assertTrue(r.ok)
+
+        r = requests.get('https://cloud.toerb.de/apps/dashboard/')
+        self.assertFalse(r.ok)
+        self.assertEqual(r.status_code, 401)
+
+    def test_gitlab(self):
+        r = requests.get('https://gitlab.toerb.de/')
+        self.assertTrue(r.ok)
+
+        r = requests.get('https://gitlab.toerb.de/toerb')
+        self.assertIn('tobiasgross.eu', r.text)
+
+    def test_static(self):
+        r = requests.get('https://tobiasgross.eu/')
+        self.assertTrue(r.ok)
+        self.assertIn('tobiasgross.eu', r.text)
+
+        r = requests.get('https://www.tobiasgross.eu/')
+        self.assertTrue(r.ok)
+        self.assertIn('tobiasgross.eu', r.text)
+
+        r = requests.get('https://mariesalm.de/')
+        self.assertTrue(r.ok)
+        self.assertIn('mariesalm.de', r.text)
+
+        r = requests.get('https://www.mariesalm.de/')
+        self.assertTrue(r.ok)
+        self.assertIn('mariesalm.de', r.text)
+
+        r = requests.get('https://things.toerb.de/test.txt')
+        self.assertTrue(r.ok)
+        self.assertEqual(r.text, 'success')
